3cb1cf0c4d3c44dc12cad4f1ec67126f9f295320
shashwot2 <49743516+shashwot2@users.noreply.github.com>
2021-11-25 15:16:12 +0700

    Create PigLatin.py

---
+++ PigLatin.py
@@ -0,0 +1,27 @@
+def pig_it(text):# problem derived from https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/python
+    s = text.split(" ")"""Note that this solution is way below optimal space or time complexity, I'll have to revise it at some point and is only meant as a solution that might
+    be easier to understand because of it's similarlity to other language syntactical usage"""
+    x = []
+    b = ""
+    specials = "[@_!#$%^&*()<>?/\|}{~:]" #list contaning all the special elements
+    for i in range(len(s)):
+        temp = ""
+        if len(s[i]) ==1 and s[i] in specials: #since our code below on line 13 would not copy to temp if the range is just 1 
+            x.append(s[i])
+        elif len(s[i]) ==1 and s[i] not in specials:
+            x.append(s[i])
+            x.append("ay")
+        for j in range((len(s[i]))-1):
+                temp = s[i][0]
+                x.append(s[i][j+1])
+        x.append(temp)
+        if temp not in specials:
+            x.append("ay")
+        if i < len(s)-1:
+            x.append(" ")
+        b = "".join(x)
+    
+    return b 
+
+
+pig_it("Hello There")
